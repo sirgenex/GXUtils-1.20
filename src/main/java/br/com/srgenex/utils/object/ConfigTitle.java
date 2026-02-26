80fdfed73142a4f82c031bd453fe19231055de41
GeneX <unconfigured@null.spigotmc.org>
2026-02-26 07:37:09 -0300

    Added ConfigTitle#send methods

--- src/main/java/br/com/srgenex/utils/object/ConfigTitle.java
+++ src/main/java/br/com/srgenex/utils/object/ConfigTitle.java
@@ -1,10 +1,13 @@
 package br.com.srgenex.utils.object;
 
+import br.com.srgenex.utils.placeholder.ReplacementPlaceholder;
 import lombok.Data;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.kyori.adventure.title.Title;
 import org.bukkit.entity.Player;
+
+import java.util.List;
 
 @Data
 public class ConfigTitle {
@@ -18,6 +21,18 @@ public class ConfigTitle {
         for (String replacement : replacements) {
             t1 = t1.replace(replacement.split(", ")[0], replacement.split(", ")[1]);
             t2 = t2.replace(replacement.split(", ")[0], replacement.split(", ")[1]);
+        }
+        Component t1Component = LegacyComponentSerializer.legacyAmpersand().deserialize(t1);
+        Component t2Component = LegacyComponentSerializer.legacyAmpersand().deserialize(t2);
+        p.showTitle(Title.title(t1Component, t2Component));
+    }
+
+    public void send(Player p, List<ReplacementPlaceholder> replacements){
+        String t1 = title;
+        String t2 = subtitle;
+        for (ReplacementPlaceholder replacement : replacements) {
+            t1 = t1.replace(replacement.getPlaceholder(), replacement.getReplacement());
+            t2 = t2.replace(replacement.getPlaceholder(), replacement.getReplacement());
         }
         Component t1Component = LegacyComponentSerializer.legacyAmpersand().deserialize(t1);
         Component t2Component = LegacyComponentSerializer.legacyAmpersand().deserialize(t2);
