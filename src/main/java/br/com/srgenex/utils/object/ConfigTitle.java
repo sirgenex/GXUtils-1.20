97c9185aa2536779ce32864b238b604e2edd88a5
GeneX <unconfigured@null.spigotmc.org>
2026-02-26 07:36:32 -0300

    Added ConfigTitle#send method

--- src/main/java/br/com/srgenex/utils/object/ConfigTitle.java
+++ src/main/java/br/com/srgenex/utils/object/ConfigTitle.java
@@ -1,11 +1,27 @@
 package br.com.srgenex.utils.object;
 
 import lombok.Data;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.kyori.adventure.title.Title;
+import org.bukkit.entity.Player;
 
 @Data
 public class ConfigTitle {
 
     private final String title;
     private final String subtitle;
+
+    public void send(Player p, String... replacements){
+        String t1 = title;
+        String t2 = subtitle;
+        for (String replacement : replacements) {
+            t1 = t1.replace(replacement.split(", ")[0], replacement.split(", ")[1]);
+            t2 = t2.replace(replacement.split(", ")[0], replacement.split(", ")[1]);
+        }
+        Component t1Component = LegacyComponentSerializer.legacyAmpersand().deserialize(t1);
+        Component t2Component = LegacyComponentSerializer.legacyAmpersand().deserialize(t2);
+        p.showTitle(Title.title(t1Component, t2Component));
+    }
 
 }
